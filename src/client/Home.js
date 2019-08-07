daa0086c7db65480e55fce1707f166b9a5482d50
behuFTP <benjamin.dals.hughes@gmail.com>
2019-08-07 13:31:35 +0200

    init commit

---
+++ src/client/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import { Link } from "react-router-dom";
+
+class Home extends Component {
+  render() {
+    return <section>hello</section>;
+  }
+}
+
+export default Home;
